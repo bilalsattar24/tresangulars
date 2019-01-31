9536bbb8182fc2cfb078abbfda0b5b3406fb91bd
Tida Sooreechine <tidasooreechine@TSOOREECHINE-MBP-SF.local>
2019-01-31 15:03:19 -0600

    added accordion component to app

--- unity/src/app/app.module.ts
+++ unity/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CdpComponent } from './cdp/cdp.component';
+import { AccordionComponent } from './accordion/accordion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CdpComponent
+    CdpComponent,
+    AccordionComponent
   ],
   imports: [
     BrowserModule,

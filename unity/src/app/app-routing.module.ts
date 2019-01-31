e9f3ccd078da4ebcb0bb2a41bfc100f07abc668a
marcelleslowery <marcelleslowery@MLOWERY-MBP-ATL.local>
2019-01-31 16:22:05 -0600

    cleaning up

--- unity/src/app/app-routing.module.ts
+++ unity/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import {LoginComponent} from './login/login.component';
-import {DashboardComponent} from './dashboard/dashboard.component'; //convert to dashboardComponent
+import {DashboardComponent} from './dashboard/dashboard.component';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path: ""  ,   redirectTo:'dashboard', pathMatch: 'full'},
+  {path: ""  ,   redirectTo:'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
-  {path: 'dashboard', component: DashboardComponent} //fix here too
+  {path: 'dashboard', component: DashboardComponent}
 ];
 
 @NgModule({

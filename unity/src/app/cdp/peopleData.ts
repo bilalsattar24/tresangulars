f1e5f9c96600d6a19c289e7ac73dc71945458857
Tida Sooreechine <tidasooreechine@TSOOREECHINE-MBP-SF.local>
2019-02-01 10:51:40 -0600

    Added cp2 ppl

--- unity/src/app/cdp/peopleData.ts
+++ unity/src/app/cdp/peopleData.ts
@@ -1,15 +1,21 @@
 export let peopleData = [
-    {cdp: "1", title: "Bilal Sattar", cohort: "C1", market: "Los Angeles", period: "Q2/Q4", image: "https://ih1.redbubble.net/image.561591662.7204/flat,1000x1000,075,f.u2.jpg", slack: "slack://user?team=T024F3C1G&id=UEJ577LFL"},
     {cdp: "1", title: "Tida Sooreechine", cohort: "C1", market: "San Francisco", period: "Q1/Q3", image: "https://www.calibre.org.uk/image-cache/harryhead_2424_233_233_centre.jpg", slack: "slack://user?team=T024F3C1G&id=UCZSE0JCA"},
+    {cdp: "1", title: "Bilal Sattar", cohort: "C1", market: "Los Angeles", period: "Q2/Q4", image: "https://ca.slack-edge.com/T024F3C1G-UEJ577LFL-3c2cff46c5da-72", slack: "slack://user?team=T024F3C1G&id=UEJ577LFL"},
     {cdp: "1", title: "Marcelles Lowery", cohort: "C2", market: "Atlanta", period: "Q1/Q3", image: "https://www.logolynx.com/images/logolynx/4b/4beebce89d681837ba2f4105ce43afac.png", slack: "slack://user?team=T024F3C1G&id=UF1JJQWT0"},
 
-    {cdp: "2", title: "Alex Rogers", cohort: "C1", market: "Atlanta", period: "Q2/Q4", image: "https://ih1.redbubble.net/image.561591662.7204/flat,1000x1000,075,f.u2.jpg", slack: "slack://user?team=T024F3C1G&id=UEJ577LFL"},
-    {cdp: "2", title: "Charmaine Estacio", cohort: "C2", market: "San Francisco", period: "Q1/Q3", image: "https://www.calibre.org.uk/image-cache/harryhead_2424_233_233_centre.jpg", slack: "slack://user?team=T024F3C1G&id=UCZSE0JCA"},
-    {cdp: "2", title: "Christian Cuellar", cohort: "C1", market: "San Francisco", period: "Q1/Q3", image: "https://www.logolynx.com/images/logolynx/4b/4beebce89d681837ba2f4105ce43afac.png", slack: "slack://user?team=T024F3C1G&id=UF1JJQWT0"},
-    {cdp: "2", title: "Jake Altabef", cohort: "C1", market: "New York", period: "Q2/Q4", image: "https://ih1.redbubble.net/image.561591662.7204/flat,1000x1000,075,f.u2.jpg", slack: "slack://user?team=T024F3C1G&id=UEJ577LFL"},
-    {cdp: "2", title: "Joel Eliason", cohort: "C2", market: "San Francisco", period: "Q1/Q3", image: "https://www.calibre.org.uk/image-cache/harryhead_2424_233_233_centre.jpg", slack: "slack://user?team=T024F3C1G&id=UCZSE0JCA"},
-    {cdp: "2", title: "Jordan Buchanan", cohort: "C1", market: "San Francisco", period: "Q1/Q3", image: "https://www.logolynx.com/images/logolynx/4b/4beebce89d681837ba2f4105ce43afac.png", slack: "slack://user?team=T024F3C1G&id=UF1JJQWT0"},
-
-
-
+    {cdp: "2", title: "Christian Cuellar", cohort: "C1", market: "San Francisco", period: "Q1/Q3", image: "https://www.logolynx.com/images/logolynx/4b/4beebce89d681837ba2f4105ce43afac.png", slack: "slack://user?team=T024F3C1G&id=UFAB1J073"},
+    {cdp: "2", title: "Charmaine Estacio", cohort: "C2", market: "San Francisco", period: "Q1/Q3", image: "https://ca.slack-edge.com/T024F3C1G-UFABL8XPB-3d8b2526f82a-72", slack: "slack://user?team=T024F3C1G&id=UFABL8XPB"},
+    {cdp: "2", title: "Alex Rogers", cohort: "C1", market: "Atlanta", period: "Q2/Q4", image: "https://ca.slack-edge.com/T024F3C1G-UFEJ47HQA-87b9e2613c9a-72", slack: "slack://user?team=T024F3C1G&id=UFEJ47HQA"},
+    {cdp: "2", title: "Jake Altabef", cohort: "C1", market: "New York", period: "Q2/Q4", image: "https://www.logolynx.com/images/logolynx/4b/4beebce89d681837ba2f4105ce43afac.png", slack: "slack://user?team=T024F3C1G&id=UEJ577LFL"},
+    {cdp: "2", title: "Jordan Buchanan", cohort: "C1", market: "Houston", period: "Q1/Q3", image: "https://www.logolynx.com/images/logolynx/4b/4beebce89d681837ba2f4105ce43afac.png", slack: "slack://user?team=T024F3C1G&id=UF1JJQWT0"},
+    {cdp: "2", title: "Jorge Garcia-Rivera", cohort: "C1", market: "Houston", period: "Q1/Q3", image: "https://www.logolynx.com/images/logolynx/4b/4beebce89d681837ba2f4105ce43afac.png", slack: "slack://user?team=T024F3C1G&id=UF1JJQWT0"},
+    {cdp: "2", title: "Juan Vasquez", cohort: "C1", market: "Dallas", period: "Q1/Q3", image: "https://www.logolynx.com/images/logolynx/4b/4beebce89d681837ba2f4105ce43afac.png", slack: "slack://user?team=T024F3C1G&id=UF1JJQWT0"},
+    {cdp: "2", title: "Katie Lutz", cohort: "C1", market: "Chicago", period: "Q1/Q3", image: "https://ca.slack-edge.com/T024F3C1G-UFG5AGJQZ-b015005d9140-72", slack: "slack://user?team=T024F3C1G&id=UFG5AGJQZ"},
+    {cdp: "2", title: "Samuel Yapkowitz", cohort: "C1", market: "Dallas", period: "Q1/Q3", image: "https://www.logolynx.com/images/logolynx/4b/4beebce89d681837ba2f4105ce43afac.png", slack: "slack://user?team=T024F3C1G&id=UF1JJQWT0"},
+    {cdp: "2", title: "Stephen Godderidge", cohort: "C1", market: "Philadelphia", period: "Q1/Q3", image: "https://www.logolynx.com/images/logolynx/4b/4beebce89d681837ba2f4105ce43afac.png", slack: "slack://user?team=T024F3C1G&id=UF1JJQWT0"},
+    {cdp: "2", title: "Trace Tschida", cohort: "C1", market: "Philadelphia", period: "Q1/Q3", image: "https://ca.slack-edge.com/T024F3C1G-UAN6UM79A-5dbaf0159594-72", slack: "slack://user?team=T024F3C1G&id=UAN6UM79A"},
+    {cdp: "2", title: "Trevor Mason", cohort: "C1", market: "Philadelphia", period: "Q1/Q3", image: "https://www.logolynx.com/images/logolynx/4b/4beebce89d681837ba2f4105ce43afac.png", slack: "slack://user?team=T024F3C1G&id=UF1JJQWT0"},
+    {cdp: "2", title: "Tristan Miller", cohort: "C1", market: "Philadelphia", period: "Q1/Q3", image: "https://www.logolynx.com/images/logolynx/4b/4beebce89d681837ba2f4105ce43afac.png", slack: "slack://user?team=T024F3C1G&id=UF1JJQWT0"},
+    {cdp: "2", title: "Usman Siddiqui", cohort: "C1", market: "Philadelphia", period: "Q1/Q3", image: "https://www.logolynx.com/images/logolynx/4b/4beebce89d681837ba2f4105ce43afac.png", slack: "slack://user?team=T024F3C1G&id=UF1JJQWT0"},
+    {cdp: "2", title: "Joel Eliason", cohort: "C2", market: "Chicago", period: "Q1/Q3", image: "https://www.logolynx.com/images/logolynx/4b/4beebce89d681837ba2f4105ce43afac.png", slack: "slack://user?team=T024F3C1G&id=UCZSE0JCA"},
 ]
